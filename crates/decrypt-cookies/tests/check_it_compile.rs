01160712342496fb8c0831dcf9c32cebfc492213
saying121 <saying121@example.com>
2024-03-31 21:36:39 +0800

    test(add): check it can compile

---
+++ crates/decrypt-cookies/tests/check_it_compile.rs
@@ -0,0 +1,40 @@
+use decrypt_cookies::{Browser, ChromiumBuilder};
+use strum::IntoEnumIterator;
+
+#[tokio::test]
+async fn can_compile() {
+    for ele in Browser::iter() {
+        dbg!(ele);
+        let Ok(getter) = ChromiumBuilder::new(ele)
+            .build()
+            .await
+        else {
+            continue;
+        };
+        let res = match getter.get_logins_all().await {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("{e}");
+                vec![]
+            },
+        };
+        match res.first() {
+            Some(first) => {
+                println!(
+                    "{} {} {} ",
+                    first.origin_url,
+                    first
+                        .username_value
+                        .as_deref()
+                        .unwrap_or_default(),
+                    first
+                        .password_value
+                        .as_deref()
+                        .unwrap_or_default()
+                );
+            },
+            None => continue,
+        };
+        println!("=============");
+    }
+}
