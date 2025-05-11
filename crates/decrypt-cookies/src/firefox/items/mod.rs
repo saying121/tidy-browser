e67dfd8380482316b5e8425930e860a8236c4f4d
saying <saying121@outlook.com>
2025-05-11 16:13:01 +0800

    refactor: remove unnecessary ref

--- crates/decrypt-cookies/src/firefox/items/mod.rs
+++ crates/decrypt-cookies/src/firefox/items/mod.rs
@@ -5,19 +5,19 @@ pub mod cookie;
 
 // reference: https://support.moonpoint.com/network/web/browser/firefox/sqlite_cookies.php
 pub(super) trait I64ToMozTime {
-    fn micros_to_moz_utc(&self) -> Option<DateTime<Utc>>;
-    fn secs_to_moz_utc(&self) -> Option<DateTime<Utc>>;
+    fn micros_to_moz_utc(self) -> Option<DateTime<Utc>>;
+    fn secs_to_moz_utc(self) -> Option<DateTime<Utc>>;
 }
 
 impl I64ToMozTime for i64 {
-    fn micros_to_moz_utc(&self) -> Option<DateTime<Utc>> {
-        match Utc.timestamp_micros(*self) {
+    fn micros_to_moz_utc(self) -> Option<DateTime<Utc>> {
+        match Utc.timestamp_micros(self) {
             LocalResult::Single(time) => Some(time),
             LocalResult::Ambiguous(..) | LocalResult::None => None,
         }
     }
-    fn secs_to_moz_utc(&self) -> Option<DateTime<Utc>> {
-        match Utc.timestamp_opt(*self, 0) {
+    fn secs_to_moz_utc(self) -> Option<DateTime<Utc>> {
+        match Utc.timestamp_opt(self, 0) {
             LocalResult::Single(time) => Some(time),
             LocalResult::Ambiguous(..) | LocalResult::None => None,
         }
