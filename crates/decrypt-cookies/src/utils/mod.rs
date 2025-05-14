786bb0cde2e4e3d5cc1723770b0e6f9539a42e8b
saying <saying121@outlook.com>
2025-05-14 18:45:24 +0800

    feat(utils): add `need_sep` fn

--- crates/decrypt-cookies/src/utils/mod.rs
+++ crates/decrypt-cookies/src/utils/mod.rs
@@ -1,1 +1,31 @@
+use std::path::Path;
+
 pub mod binary_cookies;
+
+pub fn need_sep(path: &Path) -> bool {
+    let buf = path.as_os_str().as_encoded_bytes();
+    buf.last()
+        .is_some_and(|&c| char::from(c) != std::path::MAIN_SEPARATOR)
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{path::PathBuf, str::FromStr};
+
+    use super::*;
+
+    #[test]
+    fn need_sep_test() {
+        let Ok(path) = PathBuf::from_str("/abc/");
+        assert!(!need_sep(&path));
+
+        let Ok(path) = PathBuf::from_str("/abc");
+        assert!(need_sep(&path));
+
+        let Ok(path) = PathBuf::from_str("/a/b/c");
+        assert!(need_sep(&path));
+
+        let Ok(path) = PathBuf::from_str("/a/b/c/");
+        assert!(!need_sep(&path));
+    }
+}
