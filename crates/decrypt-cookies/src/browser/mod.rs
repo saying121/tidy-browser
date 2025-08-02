265860b7f1c6afbb160f62fc4a34880835303dc9
saying <saying121@outlook.com>
2025-08-02 15:12:23 +0800

    feat(decrypt-cookies): add zen browser

--- crates/decrypt-cookies/src/browser/mod.rs
+++ crates/decrypt-cookies/src/browser/mod.rs
@@ -271,11 +271,14 @@ chromium!("windows", Yandex  , base: r"AppData\Local\Yandex\YandexBrowser\User D
 firefox!("linux", Firefox, base: ".mozilla/firefox");
 firefox!("linux", Librewolf, base: ".librewolf");
 firefox!("linux", Floorp, base: ".floorp");
+firefox!("linux", Zen, base: ".zen");
 
 firefox!("macos", Firefox, base: "Library/Application Support/Firefox");
 firefox!("macos", Librewolf, base: "Library/Application Support/librewolf");
 firefox!("macos", Floorp, base: "Library/Application Support/Floorp");
+firefox!("macos", Zen, base: "Library/Application Support/zen");
 
 firefox!("windows", Firefox, base: r"AppData\Roaming\Mozilla\Firefox");
 firefox!("windows", Librewolf, base: r"AppData\Roaming\librewolf");
 firefox!("windows", Floorp, base: r"AppData\Roaming\Floorp");
+firefox!("windows", Zen, base: r"AppData\Roaming\zen");
